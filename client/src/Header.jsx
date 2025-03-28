51c301809ade08a1d01d9cc72ce323d4fadb39ba
ABDIAWAL ADEN SIAD <adenabdiawal@gmail.com>
2025-03-28 22:11:32 +0300

    Update Header.jsx

--- client/src/Header.jsx
+++ client/src/Header.jsx
@@ -5,7 +5,7 @@ import {UserContext} from "./UserContext";
 export default function Header() {
   const {setUserInfo,userInfo} = useContext(UserContext);
   useEffect(() => {
-    fetch('http://localhost:5000/profile', {
+    fetch('https://blog-mern-backend-iwd4.onrender.com', {
       credentials: 'include',
     }).then(response => {
       response.json().then(userInfo => {
@@ -15,7 +15,7 @@ export default function Header() {
   }, []);
 
   function logout() {
-    fetch('http://localhost:5000/logout', {
+    fetch('https://blog-mern-backend-iwd4.onrender.com', {
       credentials: 'include',
       method: 'POST',
     });
