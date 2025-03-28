fc4dfb2a71f4c38279dcd8bc2ce829b4ad58ccdf
ABDIAWAL ADEN SIAD <adenabdiawal@gmail.com>
2025-03-28 22:08:25 +0300

    Update EditPost.jsx

--- client/src/pages/EditPost.jsx
+++ client/src/pages/EditPost.jsx
@@ -11,7 +11,7 @@ export default function EditPost() {
   const [redirect,setRedirect] = useState(false);
 
   useEffect(() => {
-    fetch('http://localhost:5000/post/'+id)
+    fetch('https://blog-mern-backend-iwd4.onrender.com'+id)
       .then(response => {
         response.json().then(postInfo => {
           setTitle(postInfo.title);
@@ -31,7 +31,7 @@ export default function EditPost() {
     if (files?.[0]) {
       data.set('file', files?.[0]);
     }
-    const response = await fetch('http://localhost:5000/post', {
+    const response = await fetch('https://blog-mern-backend-iwd4.onrender.com', {
       method: 'PUT',
       body: data,
       credentials: 'include',
